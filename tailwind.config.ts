f7c32ca5a016d652e7b28c03572417c9e7e6fa9d
sangenshoku <richjohnregalado@gmail.com>
2024-01-20 17:33:21 +0800

    fresh install :zap:

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import { Config } from 'tailwindcss';
+
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {}
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['light']
+  }
+} satisfies Config;
